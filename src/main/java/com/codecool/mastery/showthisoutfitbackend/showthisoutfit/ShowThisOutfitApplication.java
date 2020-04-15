ce6a8e47c1efdcb234d31f8e35cfa25bc25e5126
Melange93 <lakatos.reka.veronika@gmail.com>
2020-04-15 14:23:28 +0200

    Basic settings

---
+++ src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/ShowThisOutfitApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowThisOutfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowThisOutfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/ShowThisOutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowThisOutfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
