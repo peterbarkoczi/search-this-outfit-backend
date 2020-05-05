06d481bdd53dc21d6322bb537df263ad7c37dcb8
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-05 11:01:09 +0200

    Create Clothing class fileds

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/entity/Clothing.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/entity/Clothing.java
@@ -1,4 +1,49 @@
 package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Clothing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String genderENG;
+    private String classificationENG;
+    private String subclassificationENG;
+    private String genderHUN;
+    private String classificationHUN;
+    private String subclassificationHUN;
+    private Long fashionDaysProductId;
+    private String similarProductsLink;
+    private String mainProductsLink;
+    private String fashionDaysProductName;
+    private Long priceHUF;
+    private Float priceEUR;
+    private String brand;
+    private String stockStatusENG;
+
+    @Singular
+    @OneToMany(mappedBy = "clothing",cascade = CascadeType.MERGE)
+    @EqualsAndHashCode.Exclude
+    private List<ImageLink> images = new LinkedList<>();
+
+    @Singular
+    @OneToMany(mappedBy = "clothing",cascade = CascadeType.MERGE)
+    @EqualsAndHashCode.Exclude
+    private Set<Color> colors = new HashSet<>();
+
+    private String catalogId;
+    private String productDetails;
+
 }
