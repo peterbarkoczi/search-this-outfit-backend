ee0366a9a690b426315f60c8314b6903727d3e24
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-05 11:35:44 +0200

    Create DatasetFileReaderTest

---
+++ java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/DatasetFileReaderTest.java
@@ -0,0 +1,15 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit;
+
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util.DatasetFileReader;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DatasetFileReaderTest {
+
+    private DatasetFileReader datasetFileReader;
+
+    @BeforeEach
+    public void init() {
+        datasetFileReader = new DatasetFileReader();
+    }
+
+}
