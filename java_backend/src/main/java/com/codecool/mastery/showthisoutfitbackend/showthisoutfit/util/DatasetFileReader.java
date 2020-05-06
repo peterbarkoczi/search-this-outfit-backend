b0f527c2b785f24d01c38480f1f6cbc625d6ed63
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-06 11:26:49 +0200

    Create first unit tests

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/util/DatasetFileReader.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/util/DatasetFileReader.java
@@ -1,8 +1,43 @@
 package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
 @Component
+@Slf4j
 public class DatasetFileReader {
+
+    public List<String> readFile(String filePath) throws FileNotFoundException {
+        filePath = filePath.trim();
+
+        if (filePath.isEmpty()) {
+            throw new FileNotFoundException("File path not found: " + filePath);
+        }
+
+        List<String> result = new LinkedList<>();
+        String row = "";
+        int headerRow = 1;
+        int rowCounter = 0;
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+            while ((row = bufferedReader.readLine()) != null) {
+                rowCounter++;
+                if (rowCounter == headerRow) {
+                    continue;
+                }
+                result.add(row);
+            }
+
+        } catch (IOException e) {
+            log.error("File reading failed: " + e);
+        }
+
+        return result;
+    }
 
 }
