b1a5babd92828dd33c82de77c3ca2817b35a944b
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-07 15:29:23 +0200

    Finished Initializer class implementation what upload data to Database

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/init/Initializer.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/init/Initializer.java
@@ -1,9 +1,14 @@
 package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.init;
 
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.Clothing;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.Color;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.ImageLink;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.respository.ClothingRepository;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.respository.ColorRepository;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.respository.ImageLinkRepository;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util.DatasetFileReader;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -16,18 +21,42 @@ import java.util.*;
 @Profile("production")
 public class Initializer {
 
-    private static final String DATASET_MAIN_FOLDER_PATH = "src/main/resources/static/databasedata/";
+    @Autowired
+    private ClothingRepository clothingRepository;
 
+    @Autowired
+    private ColorRepository colorRepository;
+
+    @Autowired
+    private ImageLinkRepository imageLinkRepository;
+
+    @Autowired
     private DatasetFileReader datasetFileReader;
 
+    private static final String DATASET_MAIN_FOLDER_PATH = "src/main/resources/static/databasedata/";
     private String[] datasetFileNames = {"women_dress.csv",
             "women_jeans.csv",
             "women_skirt.csv",
             "women_tshirt.csv"};
 
-    public Initializer(DatasetFileReader datasetFileReader) {
-        this.datasetFileReader = datasetFileReader;
-    }
+    private static final int GENDER_ENG = 0;
+    private static final int CLASSIFICATION_ENG = 1;
+    private static final int SUB_CLASSIFICATION_ENG = 2;
+    private static final int GENDER_HUN = 3;
+    private static final int CLASSIFICATION_HUN = 4;
+    private static final int SUB_CLASSIFICATION_HUN = 5;
+    private static final int FASHION_DAYS_PRODUCT_ID = 6;
+    private static final int SIMILAR_PRODUCT_LINK = 7;
+    private static final int MAIN_PRODUCT_LINK = 8;
+    private static final int FASHION_DAYS_PRODUCT_NAME = 9;
+    private static final int PRICE_HUN = 10;
+    private static final int PRICE_EUR = 11;
+    private static final int BRAND = 12;
+    private static final int STOCK_STATUS_ENG = 13;
+    private static final int IMAGES = 14;
+    private static final int COLORS = 15;
+    private static final int CATALOG_ID = 16;
+    private static final int PRODUCT_DETAILS = 17;
 
     @Bean
     public CommandLineRunner afterInit() {
@@ -38,27 +67,54 @@ public class Initializer {
                 for (String row : fileRows) {
                     String[] result = row.split(",");
                     Clothing clothing = new Clothing();
-                    clothing.setGenderENG(result[0]);
-                    clothing.setClassificationENG(result[1]);
-                    clothing.setSubclassificationENG(result[2]);
-                    clothing.setGenderHUN(result[3]);
-                    clothing.setClassificationHUN(result[4]);
-                    clothing.setSubclassificationHUN(result[5]);
-                    clothing.setFashionDaysProductId(Long.parseLong(result[6]));
-                    clothing.setSimilarProductsLink(result[7]);
-                    clothing.setMainProductsLink(result[8]);
-                    clothing.setFashionDaysProductName(result[9]);
-                    clothing.setPriceHUF(Long.parseLong(result[10]));
-                    clothing.setPriceEUR(Float.parseFloat(result[11]));
-                    clothing.setBrand(result[12]);
-                    clothing.setStockStatusENG(result[13]);
+                    clothingRepository.save(clothing);
 
-                    System.out.println(clothing);
-                    break;
+                    clothing.setGenderENG(result[GENDER_ENG]);
+                    clothing.setClassificationENG(result[CLASSIFICATION_ENG]);
+                    clothing.setSubclassificationENG(result[SUB_CLASSIFICATION_ENG]);
+                    clothing.setGenderHUN(result[GENDER_HUN]);
+                    clothing.setClassificationHUN(result[CLASSIFICATION_HUN]);
+                    clothing.setSubclassificationHUN(result[SUB_CLASSIFICATION_HUN]);
+                    clothing.setFashionDaysProductId(Long.parseLong(result[FASHION_DAYS_PRODUCT_ID]));
+                    clothing.setSimilarProductsLink(result[SIMILAR_PRODUCT_LINK]);
+                    clothing.setMainProductLink(result[MAIN_PRODUCT_LINK]);
+                    clothing.setFashionDaysProductName(result[FASHION_DAYS_PRODUCT_NAME]);
+                    clothing.setPriceHUF(Long.parseLong(result[PRICE_HUN]));
+                    clothing.setPriceEUR(Float.parseFloat(result[PRICE_EUR]));
+                    clothing.setBrand(result[BRAND]);
+                    clothing.setStockStatusENG(result[STOCK_STATUS_ENG]);
+
+                    List<ImageLink> imageLinks = new LinkedList<>();
+                    String[] imgs = result[IMAGES].split(" ");
+                    for (String i : imgs) {
+                        imageLinks.add(ImageLink.builder()
+                                .url(i)
+                                .clothing(clothing)
+                                .build());
+                    }
+
+                    clothing.setImages(imageLinks);
+                    imageLinkRepository.saveAll(imageLinks);
+
+                    Set<Color> colors = new HashSet<>();
+                    String[] cols = result[COLORS].split("\\?");
+                    for (String c : cols) {
+                        colors.add(Color.builder()
+                                .color(c)
+                                .clothing(clothing)
+                                .build());
+                    }
+
+                    clothing.setColors(colors);
+                    colorRepository.saveAll(colors);
+
+                    clothing.setCatalogId(result[CATALOG_ID]);
+                    //clothing.setProductDetails(result[PRODUCT_DETAILS].replaceAll("\\$", ", "));
+
+                    clothingRepository.save(clothing);
                 }
             }
-
+            log.info("The database upload is completed.");
         };
     }
-
 }
