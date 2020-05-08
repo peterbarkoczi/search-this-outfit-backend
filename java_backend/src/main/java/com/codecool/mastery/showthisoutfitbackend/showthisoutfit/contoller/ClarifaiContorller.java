b0f15a3766a05eae98c74363988cf6d3b16002bc
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-08 14:50:56 +0200

    Create ClarifaiContorller and implement getUploadImageLabels method

---
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/contoller/ClarifaiContorller.java
@@ -0,0 +1,23 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.contoller;
+
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.inputs.Image;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.outputs.Outputs;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service.ClarifaiApiService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClarifaiContorller {
+
+    @Autowired
+    private ClarifaiApiService clarifaiApiService;
+
+    @PostMapping("/picture/upload")
+    public Outputs getUploadImageLabels(@RequestBody Image base64Image) throws JsonProcessingException {
+        return clarifaiApiService.getPictureLabels(base64Image);
+    }
+
+}
