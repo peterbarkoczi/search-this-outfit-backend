54b626c6001ad9b711fbc749dd9818eb3de4db71
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-08 14:48:33 +0200

    Delete url field from model.inputs.Image

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/model/generated/inputs/Image.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/model/generated/inputs/Image.java
@@ -4,22 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Image{
 
-	@JsonProperty("url")
-	private String url;
-
 	@JsonProperty("base64")
 	private String base64;
 
-	public String getUrl(){
-		return url;
-	}
-
 	public String getBase64() {
 		return base64;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
 	}
 
 	public void setBase64(String base64) {
