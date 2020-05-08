65cff1d4552bce5a7d5103380262945354d8e10d
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-08 11:27:24 +0200

    Generated java classes from Clarifai JSON response with RoboPOJO Generator

---
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/model/generated/ModelVersion.java
@@ -0,0 +1,45 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ModelVersion{
+
+	@JsonProperty("created_at")
+	private String createdAt;
+
+	@JsonProperty("id")
+	private String id;
+
+	@JsonProperty("status")
+	private Status status;
+
+	@JsonProperty("worker_id")
+	private String workerId;
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public Status getStatus(){
+		return status;
+	}
+
+	public String getWorkerId(){
+		return workerId;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ModelVersion{" + 
+			"created_at = '" + createdAt + '\'' + 
+			",id = '" + id + '\'' + 
+			",status = '" + status + '\'' + 
+			",worker_id = '" + workerId + '\'' + 
+			"}";
+		}
+}
---
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/model/generated/Outputs.java
@@ -0,0 +1,30 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Outputs{
+
+	@JsonProperty("outputs")
+	private List<OutputsItem> outputs;
+
+	@JsonProperty("status")
+	private Status status;
+
+	public List<OutputsItem> getOutputs(){
+		return outputs;
+	}
+
+	public Status getStatus(){
+		return status;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Outputs{" + 
+			"outputs = '" + outputs + '\'' + 
+			",status = '" + status + '\'' + 
+			"}";
+		}
+}
