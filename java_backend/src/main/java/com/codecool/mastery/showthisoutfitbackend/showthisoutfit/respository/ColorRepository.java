bfc96266f190bed0430e511127b00f7b52d5b300
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-13 16:22:16 +0200

    Delete color class and associated classes

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/respository/ColorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.respository;
-
-import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ColorRepository extends JpaRepository<Color, Long> {
-}
