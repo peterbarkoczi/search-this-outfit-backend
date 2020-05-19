6de1db7f8abb52da515d9ccae063e64e00c6edd0
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-19 13:42:42 +0200

    Delete Autowired annotation and create constructor in ColorCategorizerTest class

--- java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/util/ColorCategorizerTest.java
+++ java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/util/ColorCategorizerTest.java
@@ -2,11 +2,18 @@ package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ColorCategorizerTest {
 
-    private ColorCategorizer colorCategorizer = new ColorCategorizer();
+    private DatasetFileReader datasetFileReader = new DatasetFileReader();
+
+    private ColorCategorizer colorCategorizer = new ColorCategorizer(datasetFileReader);
+
+    ColorCategorizerTest() throws IOException {
+    }
 
     @Test
     public void getColorGroupNameFromColorCatalogTestValidInput() {
