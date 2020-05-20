85d28f4f9e17586265d2228c7f2c927a74626390
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-20 14:55:07 +0200

    Change /result endpoint method in ClothingController class

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/contoller/ClothingController.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/contoller/ClothingController.java
@@ -1,11 +1,12 @@
 package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.contoller;
 
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.Clothing;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.ChosenItem;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service.ClothingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
@@ -18,7 +19,7 @@ public class ClothingController {
     private ClothingService clothingService;
 
     @PostMapping("/result")
-    public Set<Clothing> result(@RequestParam String labelName) {
-        return clothingService.getTop20ClothingByChosenLabelName(labelName);
+    public Set<Clothing> result(@RequestBody ChosenItem chosenItem) {
+        return clothingService.getTop20ClothingByChosenLabelNameAndColor(chosenItem);
     }
 }
