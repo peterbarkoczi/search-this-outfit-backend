78beaa997d62f61b21a4c1942a58ae18caa18e8b
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-20 14:53:54 +0200

    Create getTop20ClothingByChosenLabelNameAndColor method in ClothingService class

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClothingService.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClothingService.java
@@ -1,7 +1,9 @@
 package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service;
 
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.entity.Clothing;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.ChosenItem;
 import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.respository.ClothingRepository;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.util.ColorCategorizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,22 @@ public class ClothingService {
     @Autowired
     private ClothingRepository clothingRepository;
 
+    @Autowired
+    private ClarifaiApiService clarifaiApiService;
+
+    @Autowired
+    private ColorCategorizer colorCategorizer;
 
     public Set<Clothing> getTop20ClothingByChosenLabelName(String labelName) {
         return clothingRepository.findTop20ByClassificationENG(labelName);
+    }
+
+    public Set<Clothing> getTop20ClothingByChosenLabelNameAndColor(ChosenItem chosenItem) {
+        String color = clarifaiApiService.getImageDominantColor(chosenItem.getBase64EncodePicture());
+        String colorGroup = colorCategorizer.getColorGroupNameFromColorCatalog(color.trim());
+        System.out.println(color);
+        System.out.println(colorGroup);
+        return clothingRepository.findTop20ByClassificationENGAndColor(chosenItem.getItemName(), colorGroup);
     }
 
 }
