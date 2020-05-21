b29bb53d9251f7ac8c89e49c9defda403617d56e
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-21 21:48:23 +0200

    Create ClarifaiApiServiceUtilTest

---
+++ java_backend/src/test/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/util/ClarifaiApiServiceUtilTest.java
@@ -0,0 +1,50 @@
+package com.codecool.mastery.showthisoutfitbackend.showthisoutfit.service.util;
+
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.Inputs;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.InputsData;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.InputsImage;
+import com.codecool.mastery.showthisoutfitbackend.showthisoutfit.model.generated.clarifai.inputs.InputsItem;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ClarifaiApiServiceUtilTest {
+
+    private ClarifaiApiServiceUtil util = new ClarifaiApiServiceUtil();
+
+    @Test
+    void getCommonHeadersTestContentTypeApplicationJson() {
+        HttpHeaders test = util.getCommonHeaders();
+        assertThat(test.getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    void getCommonHeadersTestSetApiKey() {
+        HttpHeaders test = util.getCommonHeaders();
+        List<String> testList = new LinkedList<>();
+        testList.add("Key " + System.getenv("API_KEY"));
+        assertThat(test.get("Authorization")).isEqualTo(testList);
+    }
+
+    @Test
+    void createApiInputs() {
+        InputsImage testInputImage =  new InputsImage();
+        testInputImage.setBase64("base64");
+        Inputs apiInputs = util.createApiInputs(testInputImage);
+
+    }
+
+    @Test
+    void createLabelSetFromOutputs() {
+    }
+
+    @Test
+    void getHighestValueColorFromColorOutputs() {
+    }
+}
