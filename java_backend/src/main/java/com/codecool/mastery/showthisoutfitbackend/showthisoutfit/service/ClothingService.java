225a8a07f6b5823e561435f39ca06bf9c247b703
Melange93 <lakatos.reka.veronika@gmail.com>
2020-05-22 14:07:06 +0200

    Delete unused used getTop20ClothingByChosenLabelName method from ClothingService

--- java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClothingService.java
+++ java_backend/src/main/java/com/codecool/mastery/showthisoutfitbackend/showthisoutfit/service/ClothingService.java
@@ -21,15 +21,9 @@ public class ClothingService {
     @Autowired
     private ColorCategorizer colorCategorizer;
 
-    public Set<Clothing> getTop20ClothingByChosenLabelName(String labelName) {
-        return clothingRepository.findTop20ByClassificationENG(labelName);
-    }
-
     public Set<Clothing> getTop20ClothingByChosenLabelNameAndColor(ChosenItem chosenItem) {
         String color = clarifaiApiService.getImageDominantColor(chosenItem.getBase64EncodePicture());
         String colorGroup = colorCategorizer.getColorGroupNameFromColorCatalog(color.trim());
-        System.out.println(color);
-        System.out.println(colorGroup);
         return clothingRepository.findTop20ByClassificationENGAndColor(chosenItem.getItemName(), colorGroup);
     }
 
